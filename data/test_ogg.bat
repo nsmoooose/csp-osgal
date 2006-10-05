c724ec2dc6d354f66a6d3ba8e64495c2b49e9b1f
vr-anders <vr-anders@7a124762-d91c-0410-bf1d-f0eece3767f7>
2006-10-05 14:15:34 +0000

    More files to checkin.

---
+++ data/test_ogg.bat
@@ -0,0 +1,6 @@
+oalpp_playstream%1 22050_1chan.ogg
+oalpp_playstream%1 44100_1chan.ogg
+oalpp_playstream%1 48000_1chan.ogg
+oalpp_playstream%1 22050_2chan.ogg
+oalpp_playstream%1 44100_2chan.ogg
+oalpp_playstream%1 48000_2chan.ogg
