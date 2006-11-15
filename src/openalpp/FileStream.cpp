4ef7f2cbd2fa1682336d16daa6744b3961cc88c9
loic <loic@7a124762-d91c-0410-bf1d-f0eece3767f7>
2006-11-15 19:25:30 +0000

    Case typo in header files

--- src/openalpp/FileStream.cpp
+++ src/openalpp/FileStream.cpp
@@ -18,8 +18,8 @@
 * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA.
 */
 
-#include "openalpp/Filestreamupdater"
-#include "openalpp/Filestream"
+#include "openalpp/FileStreamUpdater"
+#include "openalpp/FileStream"
 #include "openalpp/Sample"
 #include <vorbis/vorbisfile.h>
 #include <sstream>
