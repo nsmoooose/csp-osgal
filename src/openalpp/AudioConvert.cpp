611a8e7dae8e34541029a04665d65170e46fc633
Sukender <Sukender@7a124762-d91c-0410-bf1d-f0eece3767f7>
2009-12-07 14:54:24 +0000

    From Rizzen: Fixed compile under OpenSuse.

--- src/openalpp/AudioConvert.cpp
+++ src/openalpp/AudioConvert.cpp
@@ -22,6 +22,7 @@
 
 #include <iostream>
 #include <sstream>
+#include <limits.h>
 
 /*
 * TODO: The code in this file is copied from AL source.. I did this 
@@ -1222,7 +1223,7 @@ void *ac_wave_to_pcm(void *data, ALuint *size,
 	format = (alWaveFMT_LOKI *) riffchunk.data;
 
 	/* channels */
-	if ( swap16le(format->channels) > UCHAR_MAX ) throw std::exception("ac_wave_to_pcm(): Bad number of channels");
+	if ( swap16le(format->channels) > UCHAR_MAX ) throw std::exception(std::string("ac_wave_to_pcm(): Bad number of channels"));
 	*chan = static_cast<ALubyte>( swap16le(format->channels) );
 
 	/* freq */
